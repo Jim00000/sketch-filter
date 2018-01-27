2bc67bc36b90b3d2410822d5f9fab6fe221740b6
Jim00000 <good0121good@gmail.com>
2018-01-28 04:17:51 +0800

    Add edge sketch qt gui

--- gui/include/edge_window.hpp
+++ gui/include/edge_window.hpp
@@ -18,51 +18,30 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-#ifndef __ANI_WINDOW_HPP__
-#define __ANI_WINDOW_HPP__
+#ifndef __EDGE_WINDOW_HPP__
+#define __EDGE_WINDOW_HPP__
 
-#include <QWidget>
-#include <QPushButton>
-#include <QLabel>
-#include <QLineEdit>
-#include <QSlider>
-#include <opencv2/opencv.hpp>
+#include "ani_window.hpp"
 
 namespace sketch {
     namespace gui {
-        class AniWindow : public QWidget
+        class EdgeWindow : public AniWindow
         {
 
             Q_OBJECT
 
         public:
-            AniWindow(QWidget* parent = 0);
-            ~AniWindow();
+            EdgeWindow(QWidget* parent = 0);
+            ~EdgeWindow();
         protected:
 
         private:
-            QLabel* image;
-            QPushButton* loadBtn;
-            QPushButton* genBtn;
-            QPushButton* saveBtn;
-            QLineEdit* imagepath;
-            QSlider* alpha_slider;
-            QSlider* beta_slider;
-            cv::Mat src_image;
-            cv::Mat dst_image;
-            int alpha;
-            float beta;
-            bool isImageRead;
-            bool isOutput;
+            int threshold;
 
         protected slots:
-            void load_image_path_event();
-            void save_image_event();
-            virtual void generate_image_event();
-            void change_alpha_event(const int value);
-            void change_beta_event(const int value);
+            virtual void generate_image_event() override;
+            virtual void change_threshold_event(const int value);
 
-            friend class EdgeWindow;
         };
     }
 }
---
+++ gui/src/edge_window.cpp
@@ -0,0 +1,63 @@
+/**
+ * The sketch filter based on "Sketch filter for feature extraction and
+ * rendering applications, G. Kim, T. Kim, D.-H. Lim and C. Yim"
+ *
+ * Copyright (C) 2018  Jim00000
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include <iostream>
+#include <string>
+#include <QFileDialog>
+#include "edge_window.hpp"
+#include "edge_feature_filter.hpp"
+
+using namespace cv;
+using namespace sketch;
+using namespace sketch::gui;
+
+EdgeWindow::EdgeWindow(QWidget* parent) : AniWindow(parent)
+{
+    threshold = 0;
+    beta_slider->setMaximum(255);
+    disconnect(beta_slider, &QSlider::valueChanged, this, &AniWindow::change_beta_event);
+    connect(beta_slider, &QSlider::valueChanged, this, &EdgeWindow::change_threshold_event);
+}
+
+EdgeWindow::~EdgeWindow()
+{
+
+}
+
+void EdgeWindow::change_threshold_event(const int value)
+{
+    threshold = value;
+}
+
+void EdgeWindow::generate_image_event()
+{
+    if(isImageRead == true) {
+        genBtn->setEnabled(false);
+        edge_feature_filter edge(src_image, alpha, threshold);
+        dst_image = edge.src();
+        image->setPixmap(QPixmap::fromImage(QImage(dst_image.data, dst_image.cols, dst_image.rows, dst_image.step,
+                                            QImage::Format_Grayscale8)));
+        image->setScaledContents(true);
+        image->show();
+        genBtn->setEnabled(true);
+
+        isOutput = true;
+    }
+}
